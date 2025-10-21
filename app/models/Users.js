84c02085e0beb2926f2530e236b5ff088c566087
Alex Monson <monson.alex@rocketmail.com>
2025-10-20 23:05:24 -0700

    created user model with constraints

--- app/models/Users.js
+++ app/models/Users.js
@@ -6,9 +6,27 @@ const {sequelize, DataTypes} = require("index.js")
           primaryKey: true,
           autoIncrement: true
         },
-        username: DataTypes.STRING,
-        email: DataTypes.STRING,
-        avatar: DataTypes.STRING
+        username:{
+          type: DataTypes.STRING(50),   //up to 50 characters
+          allowNull: false,             //must have a value
+          unique: true                  //no two users can have the same username
+        } ,
+        email: {
+          type: DataTypes.STRING(100), //up to 100 characters
+          allowNull:false,
+          unique: true,
+          validate:{
+            isEmail: true             //ensures valid email format
+          }
+        },
+        avatar: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          validate: {
+            isUrl: true,        //must be a valid URL format
+          },
+          defaultValue:  'https://ui-avatars.com/api/?name=User+Default&background=0D8ABC&color=fff' //default image used if no avatar is selected
+        }
       }, {});
 
       return User;
