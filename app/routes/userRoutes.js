4fcddc27843937318b861fa8f710c7fcf6285fe7
Alex Monson <monson.alex@rocketmail.com>
2025-10-24 11:10:11 -0700

    create user route

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -2,10 +2,33 @@ const express = require('express')
 const router =  express.Router()
 const { User } = require('../models/Index')
 
+// testing router
+// router.post('/users/:id', (req, res) => {
+//   console.log('Body:', req.body);
+//   console.log('Params:', req.params);
+//   console.log('Query:', req.query);
+
+//   res.send('Check your console!');
+// });
+
+router.post('/', async(req, res) => {
+  try {
+    const user = await User.create(req.body)
+    res.status(201).json(user);
+  } catch (error) {
+    console.error("Failed to create user, ", error)
+    res.status(500).json({ message: "Failed to create user, ", error: error.message})
+  }
+});
+
+
+//finds all users
 router.get('/', async(req, res) => {
   const users = await User.findAll();
   res.json(users);
 });
 
+
+// router.get()
 
 module.exports = router;
