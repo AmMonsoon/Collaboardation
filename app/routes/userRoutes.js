5ec3f8a2cf0bf9b5ac5861a62703b584403448da
Alex Monson <monson.alex@rocketmail.com>
2025-10-27 09:23:50 -0700

    added validations for create user

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -2,21 +2,35 @@ const express = require('express')
 const router =  express.Router()
 const { User } = require('../models/Index')
 
-//testing router
-router.post('/users/:id', (req, res) => {
-  console.log('Body:', req.body);
-  console.log('Params:', req.params);
-  console.log('Query:', req.query);
-
-  res.send('Check your console!');
-});
 
 
 //creates a new user
 router.post('/', async(req, res) => {
   try {
+    const {username, email} = req.body
+    //username required and cannot be an empty string
+    if(!username || username.trim() === ""){
+      console.log("username is required")
+      res.status(400).json({ message: " Username required"})
+    }
+    // checks for email and cannot be an empty string
+    if(!email || email.trim() === ""){
+    console.log("email is required")
+    res.status(400).json({ message: " Email required"})
+    }
+
+    //checks for valid email format
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if(!emailPattern.test(email)){
+      return res.status(400).json({ message: "Invalid email format"})
+    }
+    //checks for a duplicate email
+    const existingUser = User.findOne({where: {email: email}})
+    if(existingUser){
+      return res.status(409).json({ message: "Duplicate email"})
+    }
+    //creates new user
     const user = await User.create(req.body)
-    console.log("&&&&&&&&&&&&&&&&&&&&", user)
     res.status(201).json(user);
   } catch (error) {
     console.error("Failed to create user, ", error)
@@ -41,8 +55,11 @@ router.get('/', async(req, res) => {
 //find a specific user
 router.get('/:id', async (req, res) => {
   try {
-    console.log(req.params)
     const user = await User.findByPk(req.params.id)
+    if(!user){
+      console.log("User does not exist")
+      res.status(404).send()
+    }
     res.status(200).json(user)
   } catch (error) {
     console.error("Failed to find user", error);
