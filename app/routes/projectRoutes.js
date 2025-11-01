2da5f4c138a2a60bd6f8cf7e6cc728f6ab1d0310
Alex Monson <monson.alex@rocketmail.com>
2025-11-01 11:05:06 -0700

    refactored project router

--- app/routes/projectRoutes.js
+++ app/routes/projectRoutes.js
@@ -1,114 +1,19 @@
 const express = require('express')
 const router =  express.Router()
 const { User, Project} = require('../models/Index')
+const projectController = require('../controllers/projectController')
+
 
 //creates a new project
-
-router.post('/:id', async(req, res) => {
-    
-    const userId =  req.params.id
-    const { title } = req.body
-
-    try {
-        //checks if user exists
-        const user = await User.findByPk(userId)
-        if(!user){
-            return res.status(404).json({message: "User does not exist"})
-        }
-
-        //checks if title field is valid
-        if(!title || title.trim() === ""){
-           return res.status(400).json({message: "Title required"})
-        }
-
-        const project = await Project.create( {
-            title,
-            userId
-        })
-        res.status(201).json(project) 
-    } catch (error) {
-        console.error("Failed to create project")
-        res.status(500).json({message: "Failed to create project", error: error.message})
-    }
-});
-
+router.post('/:id', projectController.createProject)
 //gets one project
-
-router.get('/:id', async(req, res) => {
-    try {
-    //checks if id parameter is valid
-    const projectId = parseInt(req.params.id, 10);
-    if (isNaN(projectId)) {
-        return res.status(400).json({ message: "Invalid project ID" });
-    }
-    //checks if project exists
-    let project = await Project.findByPk(projectId)
-
-    if(!project){
-        return res.status(400).json({message: "Project does not exist"})
-    }
-
-        res.status(200).json(project)
-    } catch (error) {
-        console.error("Failed to retrieve project")
-        res.status(500).json({message: "Failed to retrieve project", error: error.message})
-    }
-})
-
+router.get('/:id', projectController.getProject)
 //gets all projects across all users
-router.get('/', async(req, res) => {
-    try {
-        let allProjects = await Project.findAll()
-        res.status(200).json(allProjects)
-    } catch (error) {
-        console.error("Failed to retrieve projects")
-        res.status(500).json({message: "Failed to retrieve projects", error: error.message})
-    }
-})
+router.get('/', projectController.getAllProjects)
 //updates a specific project
-router.patch('/:id', async(req, res) => {
-    try {
-        //checks if id parameter is valid
-        const projectId = parseInt(req.params.id, 10);
-        if(isNaN(projectId)) {
-        return res.status(400).json({ message: "Invalid project ID" });
-    }
-        //check if project exists
-        let project = await Project.findByPk(projectId)
-
-        if(!project){
-            return res.status(400).json({message: "Project does not exist"})
-        }
-        //check if title is valid and not an empty string
-        let {title} = req.body
-        if(!title || title.trim() === ""){
-            return res.status(400).json({message: "Title must be valid"})
-        }
-
-        const updatedProject = await Project.update({title}, {where: {id: projectId}})
-        res.status(200).json(updatedProject)
-    } catch (error) {
-        console.error("Failed to update project")
-        res.status(500).json({message: "Failed to update project", error: error.message})
-    }
-})
-
+router.patch('/:id', projectController.updateProject)
 //delete a project
-router.delete('/:id', async(req, res) => {
-  try {
-    const projectId = req.params.id
-    const project = await Project.findByPk(projectId)
-    //checks if project exists
-    if(!project){
-      res.status(404).json({ message: "Project does not exist"})
-    }
-    Project.destroy({where:{id: projectId}})
-    res.status(204).send()
-  } catch (error) {
-    console.error("Failed to delete project", error);
-    res.status(500).json({ message: "Failed to delete project", error: error.message})
-  }
-})
+router.delete('/:id', projectController.deleteProject)
 
 
 
