215d3852b65d1290d09cf90bec931ff7d68dd6a8
Alex Monson <monson.alex@rocketmail.com>
2026-05-30 12:09:25 -0700

    created login page pom

---
+++ collaboardation-client/tests/pages/LoginPage.js
@@ -0,0 +1,30 @@
+export class LoginPage {
+    constructor(page){
+        this.page = page
+        this.emailInput = this.page.getByLabel("Email")
+        this.passwordInput = this.page.getByLabel("Password")
+        this.submitButton = this.page.getByRole("button", {name: "Submit"})
+    }
+
+    async goto() {
+        await this.page.goto("/login")
+    }
+
+    async fillEmail(email) {
+        await this.emailInput.fill(email)
+    }
+
+    async fillPassword(password) {
+        await this.passwordInput.fill(password)
+    }
+
+    async submit() {
+        await this.submitButton.click()
+    }
+
+    async login(email, password) {
+        await this.fillEmail(email)
+        await this.fillPassword(password)
+        await this.submit()
+    }
+}
