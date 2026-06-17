dfa7d3720927ce88300e47339f9e6ac90917d4f8
Alex Monson <monson.alex@rocketmail.com>
2026-06-17 10:57:42 -0700

    backend container can now reach db container

--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    dbName: process.env.DB_NAME,
-    dbUser: process.env.DB_USER,
-    dbHost: process.env.DB_HOST,
-    dbPassword: process.env.DB_PASS
-}
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbHost: process.env.DB_HOST,
+  dbPassword: process.env.DB_PASS,
+
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  },
+};
