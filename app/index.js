7a6d967469fb436945984f5d25905698e301e77c
Alex Monson <monson.alex@rocketmail.com>
2026-07-12 08:42:29 -0700

    added CI settings for playwright tests

--- app/index.js
+++ app/index.js
@@ -1,8 +1,5 @@
-console.log("1")
 const express = require('express')
-console.log("2")
 const cookieParser = require('cookie-parser')
-console.log("3")
 const cors  = require('cors')
 process.on('uncaughtException', (err) => {
   console.error('Uncaught Exception:', err);
@@ -11,19 +8,14 @@ process.on('uncaughtException', (err) => {
 process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection:', reason);
 });
-console.log("4")
+
 const sequelize = require('./config/database')
-console.log("5")
 const userRoutes = require('./routes/userRoutes')
-console.log("6")
 const projectRoutes = require('./routes/projectRoutes')
-console.log("7")
 const boardRoutes =  require('./routes/boardRoutes')
-console.log("8")
 const taskRoutes = require('./routes/taskRoutes')
-console.log("9")
+
 const { errorHandler, notFoundHandler } = require('./middleware/index')
-console.log("10")
 const app = express();
 
 console.log("11")
@@ -46,40 +38,31 @@ app.use(
     credentials: true, 
   })
 );
-console.log("12")
+
 app.use(express.json())            //Required to allow Express to read JSON request bodies
-console.log("13")
 app.use(cookieParser())
-console.log("14")
+
 app.use('/users', userRoutes);
-console.log("15")
+
 app.use('/projects', projectRoutes);
-console.log("16")
 app.use('/projects/:projectId/boards', boardRoutes);
-console.log("17")
 app.use('/projects/:projectId/boards/:boardId/tasks', taskRoutes)
-console.log("18")
+
 app.get('/', (req, res) => {
   res.status(200).send('Server is running');
 });
-console.log("19")
+
 
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-console.log("before startup")
+
 const PORT = process.env.PORT || 3000;
 
 (async () => {
     try{
-        console.log("Starting Server")
-
-        console.log("Authenticating DB")
         await sequelize.authenticate();
-        console.log("Database authenticated");
-        console.log("Syncing Models")
         await sequelize.sync();
-        console.log("Models synced");
         app.listen(PORT , () => console.log(`Server is listening on port ${PORT}`));
     }catch(error){
         console.error('Failed to sync models: ' , error)
